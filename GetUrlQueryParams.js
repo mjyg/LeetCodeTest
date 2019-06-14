bac84962e5d6b74d050d37290c192f1ef1501a77
zhouyanjie <940095681@qq.com>
2019-06-14 09:04:33 +0800

    Get Url Query Params

---
+++ GetUrlQueryParams.js
@@ -0,0 +1,26 @@
+//查询URL中的参数
+function getUrlQueryParams(url) {
+  let args={};
+  if (!url) {
+    return args;
+  }
+
+  let qs = url.split('?');
+  if (qs.length < 2) {
+    return args;
+  }
+
+  let items = qs[1].split('&');
+  items.map((item) => {
+    let temp = item.split('=');
+    let name = decodeURIComponent(temp[0]);
+    let value = decodeURIComponent(temp[1]);
+    if (name) {
+      args[name] = value;
+    }
+  });
+
+  return args;
+}
+
+console.log(getUrlQueryParams('https://cn.bing.com/search-18A41D8D0F95C74D8?a=1&ab=2'));
